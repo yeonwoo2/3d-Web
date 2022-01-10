ea0ca60c0fbf0be257313f61f9f1c4c0bc4466f5
Hyeonw <hyeonw777@naver.com>
2022-01-10 15:26:42 +0900

    프로젝트 설정 완료

---
+++ src/main/java/toy/project/free3d/Free3dApplication.java
@@ -0,0 +1,13 @@
+package toy.project.free3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Free3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Free3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/project/free3d/Free3dApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.project.free3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Free3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
